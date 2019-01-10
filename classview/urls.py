3160f7d859c4bc072d212abd948343889d495edb
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-01-10 21:56:23 +0800

    as_view方法原理分析

--- classview/urls.py
+++ classview/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     # url(路径, 函数名)
+    # as_view --> 把方法变成函数
+    #         --> 会找到指定类中的对应方法(根据本次请求方法动态去查找类中定义的方法)
     url(r'^demoview/$', views.DemoView.as_view())
 ]
