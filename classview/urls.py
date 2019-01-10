9d5d81abdfc28015348f077b3632bace8c42a110
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-01-10 22:03:38 +0800

    在路由as_view的地方装饰类视图

--- classview/urls.py
+++ classview/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     # url(路径, 函数名)
     # as_view --> 把方法变成函数
     #         --> 会找到指定类中的对应方法(根据本次请求方法动态去查找类中定义的方法)
-    url(r'^demoview/$', views.DemoView.as_view())
+
+    # 直接在定义路由时给as_view返回回来的函数进行装饰
+    url(r'^demoview/$', views.my_decorator(views.DemoView.as_view()))
 ]
--- classview/views.py
+++ classview/views.py
@@ -5,6 +5,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+def my_decorator(func_view):
+
+    def wrapper(request, *args, **kwargs):
+        print("被装饰了")
+        return func_view(request, *args, **kwargs)
+
+    return wrapper
+
+
 class DemoView(View):
     """类视图: 类视图中的方法名必须是请求方法名字小写"""
     def get(self, request):
