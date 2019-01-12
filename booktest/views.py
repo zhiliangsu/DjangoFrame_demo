1043d7657e60f45a29c430e2086266735d350be6
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-01-12 20:53:45 +0800

    模型新增操作: save和create

--- booktest/views.py
+++ booktest/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from booktest.models import BookInfo, HeroInfo
+
+# 新增一本书
+# book = BookInfo()
+# book.btitle = '西游记'
+# book.bpub_date = '1991-11-11'
+# book.save()
+
+book = BookInfo(
+    btitle='西游记',
+    bpub_date='1991-11-11'
+)
+book.save()
+
+
+# 用create创建的时候,不用再调用save,创建的同时直接保存
+hero = HeroInfo.objects.create(
+    hname='白骨精',
+    hbook=book,
+    # hbook_id=book.id,
+)
