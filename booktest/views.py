a241111f1ab5fa0b2146be6675e13c3a25d82f2e
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-01-12 21:02:29 +0800

    数据库ORM的基本查询: get/all/count

--- booktest/views.py
+++ booktest/views.py
@@ -9,16 +9,34 @@ from booktest.models import BookInfo, HeroInfo
 # book.bpub_date = '1991-11-11'
 # book.save()
 
-book = BookInfo(
-    btitle='西游记',
-    bpub_date='1991-11-11'
-)
-book.save()
+# book = BookInfo(
+#     btitle='西游记',
+#     bpub_date='1991-11-11'
+# )
+# book.save()
+#
+#
+# # 用create创建的时候,不用再调用save,创建的同时直接保存
+# hero = HeroInfo.objects.create(
+#     hname='白骨精',
+#     hbook=book,
+#     # hbook_id=book.id,
+# )
 
 
-# 用create创建的时候,不用再调用save,创建的同时直接保存
-hero = HeroInfo.objects.create(
-    hname='白骨精',
-    hbook=book,
-    # hbook_id=book.id,
-)
+"""
+基本查询:
+    get 查询单一结果，如果不存在会抛出模型类.DoesNotExist异常。
+    all 查询多个结果。
+    count 查询结果数量。
+"""
+# book = BookInfo.objects.get(id=5)
+# book = BookInfo.objects.get(pk=3)
+try:
+    BookInfo.objects.get(pk=30)
+except BookInfo.DoesNotExist as e:
+    print("Test:", e)
+# booktest.models.DoesNotExist: BookInfo matching query does not exist.
+
+BookInfo.objects.all()
+BookInfo.objects.all().count()
